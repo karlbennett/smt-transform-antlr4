a31aa4c145ac2bbc5cc5aefc92f5369f9b8a0ab7
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-03 17:41:37 +1300

    Updated the NullTokenTransformation to extend the new NullTransformation.

--- src/main/java/shiver/me/timbers/transform/antlr4/NullTokenTransformation.java
+++ src/main/java/shiver/me/timbers/transform/antlr4/NullTokenTransformation.java
@@ -2,42 +2,18 @@ package shiver.me.timbers.transform.antlr4;
 
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.Token;
+import shiver.me.timbers.transform.NullTransformation;
 
 /**
  * This transformation should be returned when no matching transformation can be found.
  */
-public class NullTokenTransformation implements TokenTransformation {
+public class NullTokenTransformation extends NullTransformation implements TokenTransformation {
 
     public static final TokenTransformation NULL_TOKEN_TRANSFORMATION = new NullTokenTransformation();
-
-    @Override
-    public String getName() {
-
-        return NullTokenTransformation.class.getSimpleName();
-    }
 
     @Override
     public String apply(RuleContext context, Token token, String string) {
 
         return string;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        NullTokenTransformation that = (NullTokenTransformation) o;
-
-        return getName().equals(that.getName());
-
-    }
-
-    @Override
-    public int hashCode() {
-
-        return getName().hashCode();
     }
 }
