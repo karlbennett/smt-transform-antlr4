596dd0a344dba80a673061fb7b44360826062757
karlbennett <where_1ts@yahoo.com>
2014-01-15 21:07:43 +1300

    Deleted an unused constant.

--- src/main/java/shiver/me/timbers/transform/antlr4/IterableTokenTransformations.java
+++ src/main/java/shiver/me/timbers/transform/antlr4/IterableTokenTransformations.java
@@ -1,13 +1,10 @@
 package shiver.me.timbers.transform.antlr4;
 
-import shiver.me.timbers.transform.Transformations;
 import shiver.me.timbers.transform.iterable.IterableTransformations;
 
 import static shiver.me.timbers.transform.antlr4.NullTokenTransformation.NULL_TOKEN_TRANSFORMATION;
 
 public class IterableTokenTransformations extends IterableTransformations<TokenTransformation> {
-
-    public static final Transformations<TokenTransformation> EMPTY_TRANSFORMATIONS = new IterableTokenTransformations();
 
     public IterableTokenTransformations() {
         super(NULL_TOKEN_TRANSFORMATION);
