c16eebb0f3793d6f1231e2f11c05534506b51139
karlbennett <where_1ts@yahoo.com>
2014-01-15 20:37:46 +1300

    Updated the Antlr4StringTransformer with the new MIME type support.

--- src/main/java/shiver/me/timbers/transform/antlr4/Antlr4StringTransformer.java
+++ src/main/java/shiver/me/timbers/transform/antlr4/Antlr4StringTransformer.java
@@ -6,9 +6,12 @@ import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shiver.me.timbers.transform.AbstractTransformer;
 import shiver.me.timbers.transform.Transformations;
 import shiver.me.timbers.transform.antlr4.listeners.TransformingParseTreeListener;
 import shiver.me.timbers.transform.string.StringTransformer;
+
+import javax.activation.MimeType;
 
 import static shiver.me.timbers.asserts.Asserts.argumentIsNullMessage;
 import static shiver.me.timbers.asserts.Asserts.assertIsNotNull;
@@ -17,13 +20,15 @@ import static shiver.me.timbers.asserts.Asserts.assertIsNotNull;
  * This is an ANTLR4 specific {@code StreamTransformer} that can be used transform the code within the supplied input stream
  * with the use of the parser built from the dependent {@code ParserBuilder}.
  */
-public class Antlr4StringTransformer<P extends Recognizer> implements StringTransformer<TokenTransformation> {
+public class Antlr4StringTransformer<P extends Recognizer> extends AbstractTransformer<String, TokenTransformation>
+        implements StringTransformer<TokenTransformation> {
 
     private final Logger log = LoggerFactory.getLogger(Antlr4StringTransformer.class);
 
     private final ParserBuilder<P> parserBuilder;
 
-    public Antlr4StringTransformer(ParserBuilder<P> parserBuilder) {
+    public Antlr4StringTransformer(MimeType mimeType, ParserBuilder<P> parserBuilder) {
+        super(mimeType);
 
         assertIsNotNull(argumentIsNullMessage("parserBuilder"), parserBuilder);
 
--- src/test/java/shiver/me/timbers/transform/antlr4/Antlr4StringTransformerTest.java
+++ src/test/java/shiver/me/timbers/transform/antlr4/Antlr4StringTransformerTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static shiver.me.timbers.transform.NullTransformer.TEXT_PLAIN;
 
 public class Antlr4StringTransformerTest {
 
@@ -27,9 +28,21 @@ public class Antlr4StringTransformerTest {
     private static final String TEST_RULE_NAME = "test_rule";
 
     @Test
-    public void testCreateWithParserBuilder() {
+    public void testCreateWithMimeTypeAndParserBuilder() {
 
-        new Antlr4StringTransformer<Recognizer>(mockParserBuilder());
+        new Antlr4StringTransformer<Recognizer>(TEXT_PLAIN, mockParserBuilder());
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testCreateWithNullMimeTypeAndParserBuilder() {
+
+        new Antlr4StringTransformer<Recognizer>(null, mockParserBuilder());
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testCreateWithMimeTypeAndNullParserBuilder() {
+
+        new Antlr4StringTransformer<Recognizer>(TEXT_PLAIN, null);
     }
 
     @Test
@@ -39,7 +52,7 @@ public class Antlr4StringTransformerTest {
 
         final ParserBuilder<Recognizer> parserBuilder = mockParserBuilder(transformations);
 
-        new Antlr4StringTransformer<Recognizer>(parserBuilder).transform(TEST_STRING, transformations);
+        new Antlr4StringTransformer<Recognizer>(TEXT_PLAIN, parserBuilder).transform(TEST_STRING, transformations);
 
         verify(transformations, times(1)).get(TEST_TOKEN_NAME);
         verify(transformations, times(1)).get(TEST_RULE_NAME);
@@ -53,13 +66,13 @@ public class Antlr4StringTransformerTest {
     @Test(expected = AssertionError.class)
     public void testTransformWithNullStream() {
 
-        new Antlr4StringTransformer<Recognizer>(mockParserBuilder()).transform(null, mockTransformations());
+        new Antlr4StringTransformer<Recognizer>(TEXT_PLAIN, mockParserBuilder()).transform(null, mockTransformations());
     }
 
     @Test(expected = AssertionError.class)
     public void testTransformWithNullTransformations() {
 
-        new Antlr4StringTransformer<Recognizer>(mockParserBuilder()).transform(TEST_STRING, null);
+        new Antlr4StringTransformer<Recognizer>(TEXT_PLAIN, mockParserBuilder()).transform(TEST_STRING, null);
     }
 
     private static ParserBuilder<Recognizer> mockParserBuilder() {
@@ -70,28 +83,56 @@ public class Antlr4StringTransformerTest {
     private static ParserBuilder<Recognizer> mockParserBuilder(
             Transformations<TokenTransformation> tokenTransformations) {
 
+        final RuleContext context = mockRuleContext();
+
+        final Token token = mockToken();
+
+        final TerminalNode terminalNode = mockTerminalNode(context, token);
+
+        final Recognizer recognizer = mockRecognizer();
+
+        @SuppressWarnings("unchecked")
+        final ParserBuilder<Recognizer> parserBuilder = mock(ParserBuilder.class);
+        when(parserBuilder.buildParser(TEST_STRING, tokenTransformations)).thenReturn(recognizer);
+        when(parserBuilder.parse(recognizer)).thenReturn(terminalNode);
+
+        return parserBuilder;
+    }
+
+    private static RuleContext mockRuleContext() {
+
         final RuleContext context = mock(RuleContext.class);
         when(context.getRuleIndex()).thenReturn(TEST_RULE_TYPE);
+
+        return context;
+    }
+
+    private static Token mockToken() {
 
         final Token token = mock(Token.class);
         when(token.getType()).thenReturn(TEST_TOKEN_TYPE);
         when(token.getText()).thenReturn(TEST_TOKEN_NAME);
 
-        final TerminalNode parseTree = mock(TerminalNode.class);
-        when(parseTree.getParent()).thenReturn(parseTree);
-        when(parseTree.getPayload()).thenReturn(context);
-        when(parseTree.getSymbol()).thenReturn(token);
+        return token;
+    }
+
+    private static TerminalNode mockTerminalNode(RuleContext context, Token token) {
+
+        final TerminalNode terminalNode = mock(TerminalNode.class);
+        when(terminalNode.getParent()).thenReturn(terminalNode);
+        when(terminalNode.getPayload()).thenReturn(context);
+        when(terminalNode.getSymbol()).thenReturn(token);
+
+        return terminalNode;
+    }
+
+    private static Recognizer mockRecognizer() {
 
         final Recognizer recognizer = mock(Recognizer.class);
         when(recognizer.getTokenNames()).thenReturn(new String[]{TEST_TOKEN_NAME});
         when(recognizer.getRuleNames()).thenReturn(new String[]{TEST_RULE_NAME});
 
-        @SuppressWarnings("unchecked")
-        final ParserBuilder<Recognizer> parserBuilder = mock(ParserBuilder.class);
-        when(parserBuilder.buildParser(TEST_STRING, tokenTransformations)).thenReturn(recognizer);
-        when(parserBuilder.parse(recognizer)).thenReturn(parseTree);
-
-        return parserBuilder;
+        return recognizer;
     }
 
     private static Transformations<TokenTransformation> mockTransformations() {
